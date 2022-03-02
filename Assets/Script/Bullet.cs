79641af41d669454e8dbe655ac233258988f109e
mat-planchot <mat_planchot@hotmail.fr>
2022-03-02 01:12:45 +0100

    bullet and effects

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rb;
+    public int damage = 40;
+    public GameObject impactEffect;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.velocity = transform.right * speed;
+    }
+
+    void OnTriggerEnter2D(Collider2D hitInfo) {
+        Enemy enemy = hitInfo.GetComponent<Enemy>();
+        if (enemy != null) {
+            enemy.TakeDamage(damage);
+        }
+        GameObject clone = (GameObject) Instantiate(impactEffect, transform.position, transform.rotation);
+        Destroy(clone, 0.3f);
+        Destroy(gameObject);
+    }
+}
